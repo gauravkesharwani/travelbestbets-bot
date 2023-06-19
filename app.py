3d79ba526f836758ab8e8a9e865f916654065a42
Gaurav <gauravkesharwani@gmail.com>
2023-06-19 14:07:52 -0700

    added cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import chatter4
 import logging
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.static_folder = 'static'
 
 # Configure logging
