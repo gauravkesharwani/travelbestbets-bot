464033799d240c6d5801f44a169410e39d189292
Gaurav <gauravkesharwani@gmail.com>
2023-07-06 14:26:57 -0700

    changed cors

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app)
 
 
 CORS(app, origins=["https://www.travelbestbets.com", "https://travelbestbets.com"])
